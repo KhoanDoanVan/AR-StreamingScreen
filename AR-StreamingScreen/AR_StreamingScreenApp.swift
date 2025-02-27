88d3dc106706d9dce7a5860902f7395db0206e1f
khoanisdev <doanvankhoan124@gmail.com>
2025-02-27 20:01:41 +0700

    Initial Commit

---
+++ AR-StreamingScreen/AR_StreamingScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  AR_StreamingScreenApp.swift
+//  AR-StreamingScreen
+//
+//  Created by Đoàn Văn Khoan on 27/2/25.
+//
+
+import SwiftUI
+
+@main
+struct AR_StreamingScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
